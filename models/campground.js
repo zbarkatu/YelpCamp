83f97faea01cd60e5cc9cc50f4c9a41a6f8124cc
zbarkatu <zbarkatu@gmail.com>
2023-11-20 21:33:30 -0800

    Completed Campgrounds CRUD

--- models/campground.js
+++ models/campground.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = newSchema({
+const CampgroundSchema = new Schema({
     title: String,
     price: String,
     description: String,
