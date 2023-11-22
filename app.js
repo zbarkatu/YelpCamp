bdeccece4f721c5d84be201ffe6c808f37248f46
zbarkatu <zbarkatu@gmail.com>
2023-11-21 20:20:36 -0800

    Finsihed adding basic styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -13,6 +14,7 @@ db.once("open", () => {
     console.log("Database connected")
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque pellentesque suscipit. Sed consectetur sapien in metus blandit consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate nisi quis urna pretium, sit amet bibendum ipsum auctor. Donec faucibus odio at nisl mollis posuere. Pellentesque non magna lacinia, ultricies enim id, pharetra dui. Quisque id leo id magna cursus congue. Mauris eu tellus purus. Phasellus nisi mauris, dignissim in augue non, posuere vulputate enim. Praesent cursus iaculis iaculis. Vestibulum finibus dui et felis varius, nec efficitur arcu hendrerit. Aliquam placerat tempor urna non vehicula. Nullam eget ipsum vel turpis cursus volutpat.',
+            price 
         })
         await camp.save()
     }
